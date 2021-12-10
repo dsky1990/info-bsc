7b753b1c35e363439d2811d8e2d7f52100da12b5
Dsky <dutian1990@gmail.com>
2021-12-10 19:27:24 +0800

    Add files via upload

---
+++ precache-manifest.a81dfd83e97d12f49dbe7f5d5b1dc383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47caf1414d84b50c6915ade08f8c4cb",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info-bsc@main/index.html"
+  },
+  {
+    "revision": "27e62d25f65b358033fd",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info-bsc@main/static/js/2.87cd507f.chunk.js"
+  },
+  {
+    "revision": "627773ea9d4d22328b7e3c164cff9ea5",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info-bsc@main/static/js/2.87cd507f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd3543b90d20e57378d",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info-bsc@main/static/js/main.228fc6e6.chunk.js"
+  },
+  {
+    "revision": "b7d687aa9324499e6758",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info-bsc@main/static/js/runtime-main.74d77b98.js"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info-bsc@main/static/media/logo.8e72f6f2.png"
+  }
+]);
